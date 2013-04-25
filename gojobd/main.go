1fa6abec76e8ab2430af547c56c3cbde74552e04
Kevin Retzke <retzkek@gmail.com>
2013-04-24 22:38:25 -0500

    added basic command-line interface for gojobd

--- gojobd/main.go
+++ gojobd/main.go
@@ -1,14 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"github.com/retzkek/gojob"
 	"log"
 	"net"
 	"net/http"
 	"net/rpc"
+	"os"
 )
 
-func main() {
+type Command struct {
+	Command string
+	Short   string
+	Run     func(cmd *Command, args []string)
+	//Flags flag.FlagSet
+}
+
+func printHelp(exe string, commands []Command) {
+	fmt.Printf("\nusage: %s command [args]\n", exe)
+	fmt.Printf("\tcommands:\n")
+	for _, c := range commands {
+		fmt.Printf("\t\t%-10s %s\n", c.Command, c.Short)
+	}
+	fmt.Printf("\n")
+}
+
+func runHelp(cmd *Command, args []string) {
+	fmt.Println("TBA")
+}
+
+func runRpc(cmd *Command, args []string) {
 	status := new(gojob.Status)
 	rpc.Register(status)
 	rpc.HandleHTTP()
@@ -17,4 +39,31 @@ func main() {
 		log.Fatal("listen error:", e)
 	}
 	http.Serve(l, nil)
+}
+
+func runMongo(cmd *Command, args []string) {
+	fmt.Println("TBA")
+}
+
+func runTest(cmd *Command, args []string) {
+	fmt.Println("TBA")
+}
+
+func main() {
+	commands := []Command{
+		Command{"help", "command help", runHelp},
+		Command{"rpc", "run RPC server", runRpc},
+		Command{"mongo", "send status to MongoDB server", runMongo},
+		Command{"test", "run diagnostics and print to terminal", runTest}}
+	if len(os.Args) < 2 {
+		printHelp(os.Args[0], commands)
+		os.Exit(0)
+	}
+	cmd := os.Args[1]
+	for _, c := range commands {
+		if cmd == c.Command {
+			c.Run(&c, os.Args[2:])
+			break
+		}
+	}
 }
